c503d2d10200c9b9d3532ae3dbae64930806391f
NguyenTu <nngoctu004@gmail.com>
2023-02-14 10:31:19 +0700

    delete product

--- src/main/java/com/example/be_food_order/controller/product/ImageController.java
+++ src/main/java/com/example/be_food_order/controller/product/ImageController.java
@@ -3,6 +3,8 @@ package com.example.be_food_order.controller.product;
 import com.example.be_food_order.model.product.Image;
 import com.example.be_food_order.model.product.Product;
 import com.example.be_food_order.service.product.ImageService;
+import com.example.be_food_order.service.product.ProductMethodService;
+import com.example.be_food_order.service.product.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/images")
@@ -17,6 +20,10 @@ import java.util.List;
 public class ImageController {
     @Autowired
     private ImageService imageService;
+    @Autowired
+    private ProductService productService;
+    @Autowired
+    private ProductMethodService productMethodService;
     @PostMapping("create")
     public ResponseEntity<Image> createProduct(@RequestBody Image image){
         return new ResponseEntity<>(imageService.save(image), HttpStatus.CREATED);
@@ -41,5 +48,21 @@ public class ImageController {
     @GetMapping("/filters")
     public ResponseEntity<Iterable<Image>> findAllFilter(){
         return new ResponseEntity<>(imageService.findAllFilter(), HttpStatus.OK);
+    }
+    @DeleteMapping("/delete/product/{id}")
+    public ResponseEntity<String> deleteAllByProduct(@PathVariable Long id){
+        Optional<Product> product = productService.findOneById(id);
+        if(product.isPresent()) {
+            boolean checkImg = imageService.deleteAllByProduct(product.get().getId());
+            boolean checkProduct = productService.deleteProduct(product.get().getId());
+            boolean checkProductMethod = productMethodService.deleteProductMethod(product.get().getProductMethod().getId());
+            if (checkImg && checkProduct && checkProductMethod){
+                return new ResponseEntity<>("done",HttpStatus.OK);
+            }else{
+                return new ResponseEntity<>("error",HttpStatus.NOT_FOUND);
+            }
+        }else{
+            return new ResponseEntity<>("error",HttpStatus.NOT_FOUND);
+        }
     }
 }
