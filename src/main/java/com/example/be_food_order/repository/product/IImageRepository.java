bebe5daea697ef9fe81e79cc4073fcae6ccb769a
NguyenTu <nngoctu004@gmail.com>
2023-02-22 00:06:58 +0700

    class pojo filter

--- src/main/java/com/example/be_food_order/repository/product/IImageRepository.java
+++ src/main/java/com/example/be_food_order/repository/product/IImageRepository.java
@@ -18,7 +18,10 @@ public interface IImageRepository extends JpaRepository<Image, Long> {
     @Query(value = "select * from image group by image.product_id", nativeQuery = true)
     Iterable<Image> findAllFilter();
 
-    @Query(value = "select * from image join product on image.product_id = product.id where product.store_id= :storeId group by image.product_id", nativeQuery = true)
+    @Query(value = "select * from " +
+            "image join product on image.product_id = product.id " +
+            "where product.store_id= :storeId " +
+            "group by image.product_id", nativeQuery = true)
     Iterable<Image> findAllFilterStore(@Param("storeId") Long storeId);
 
     @Transactional
@@ -26,12 +29,25 @@ public interface IImageRepository extends JpaRepository<Image, Long> {
     @Query(value = "delete from image where image.product_id= :productId", nativeQuery = true)
     void deleteListImageByProduct(@Param("productId") Long productId);
 
-    @Query(value = "SELECT i.id, i.name, i.product_id FROM Image i  JOIN  product p on i.product_id = p.id JOIN product_method pm on p.product_method_id = pm.id  and pm.category_id = :category_id group by  i.product_id", nativeQuery = true)
+    @Query(value = "SELECT i.id, i.name, i.product_id " +
+            "FROM" +
+            " Image i  JOIN  product p on i.product_id = p.id " +
+            "JOIN product_method pm on p.product_method_id = pm.id  " +
+            "and" +
+            " pm.category_id = :category_id " +
+            "group by  i.product_id", nativeQuery = true)
     Iterable<Image> findAllByCategoryId(@Param("category_id") Long id);
 
     Iterable<Image> findAllByProductNameContains(String name);
 
-    @Query(value = "select i.id, i.name, i.product_id from image i join  product p on i.product_id = p.id join product_method pm on p.product_method_id = pm.id  group by i.product_id order by pm.quantity desc", nativeQuery = true)
+    @Query(value = "select i.id, i.name, i.product_id " +
+            "from" +
+            " image i join  product p on i.product_id = p.id " +
+            "join product_method pm on p.product_method_id = pm.id  " +
+            "group by" +
+            " i.product_id " +
+            "order by" +
+            " pm.quantity desc", nativeQuery = true)
     Iterable<Image> findAllTopFood();
 
 
